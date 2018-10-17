311ae8d0ee870119f407a7f2d697fb5138cdad6a
Ross <ross@sov.tech>
2018-10-17 21:38:38 +0200

    Updated gallery

--- src/components/gallery1.vue
+++ src/components/gallery1.vue
@@ -5,7 +5,8 @@
       <div class="__content" style="text-align:center">
         <h1><span class="highlight">Product Gallery</span></h1>
         <p style="font-size: 1.4rem;">
-          PolyTherm offers a wide range of plastic recycling machinery to suit your individual needs
+          PolyTherm offers several products from plastic recycling machinery, extrusion lines and auxilary machines.
+           Checkout some of our products below.
         </p>
       </div>
     </div>
@@ -34,6 +35,30 @@ export default {
   data() {
     return{
       images: [
+      require('../assets/images/aus/1.jpg'),
+      require('../assets/images/aus/2.jpg'),
+      require('../assets/images/aus/3.jpg'),
+      require('../assets/images/aus/4.jpg'),
+      require('../assets/images/aus/5.jpg'),
+      require('../assets/images/aus/6.jpg'),
+      require('../assets/images/aus/7.jpg'),
+      require('../assets/images/aus/8.jpg'),
+      require('../assets/images/aus/9.jpg'),
+      require('../assets/images/aus/10.jpg'),
+      require('../assets/images/aus/11.jpg'),
+      require('../assets/images/aus/12.jpg'),
+      require('../assets/images/aus/13.jpg'),
+      require('../assets/images/aus/14.jpg'),
+      require('../assets/images/aus/15.jpg'),
+      require('../assets/images/aus/16.jpg'),
+      require('../assets/images/aus/17.jpg'),
+      require('../assets/images/aus/18.jpg'),
+      require('../assets/images/aus/19.jpg'),
+      require('../assets/images/aus/20.jpg'),
+      require('../assets/images/aus/21.jpg'),
+      require('../assets/images/aus/22.jpg'),
+      require('../assets/images/aus/23.jpg'),
+      require('../assets/images/aus/24.jpg'),
       require('../assets/images/pet-bottle-washing.jpg'),
       require('../assets/images/Label remover 1.jpg'),
       require('../assets/images/label remover 2.jpg'),
